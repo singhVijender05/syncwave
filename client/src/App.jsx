89b79d2dae4211f250595259c09f6250fe31b0aa
vishalchaurasia10 <1ms21cs138@msrit.edu>
2024-09-05 23:15:39 +0530

    feat: created login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Login from './components/Auth/Login'
-import Room from './pages/Room'
-import Dashboard from './pages/Dashboard'
-import SignUp from './components/Auth/Signup'
-import OAuthCallback from './components/Auth/OAuthCallback'
+// import Room from './pages/Room'
+// import Dashboard from './pages/Dashboard'
+// import SignUp from './components/Auth/Signup'
+// import OAuthCallback from './components/Auth/OAuthCallback'
+import SignInPage from './pages/sign-in/SignInPage'
+import { Toaster } from 'react-hot-toast'
 function App() {
-  
+
   return (
-    <div className="App">
+    <div className="">
+      <Toaster />
       <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/oauth/callback" element={<OAuthCallback/>} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/room' element={<Room />} />
-        </Routes>
+        <Route path="/sign-in" element={<SignInPage />} />
+        {/* <Route path="/oauth/callback" element={<OAuthCallback />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/room' element={<Room />} /> */}
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/sign-in/SignIn.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react'
+import { FcGoogle } from 'react-icons/fc'
+import { Link, useNavigate } from 'react-router-dom'
+import useAuthStore from '../../store/Auth'
+
+const SignIn = () => {
+    const [credentials, setCredentials] = useState({ email: '', password: '' })
+    const navigate = useNavigate()
+    const { login } = useAuthStore()
+
+    const handleChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+
+    const handleSignin = async () => {
+        await login(credentials, navigate)
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: 'smooth'
+            });
+        }, 1000);
+
+        // Clean up the timeout if the component unmounts
+        return () => clearTimeout(timer);
+    }, []);
+
+
+    return (
+        <>
+            <div className='flex pb-28 lg:pb-0 px-8 md:px-40 lg:px-0 py-10 md:py-16 lg:pt-12 lg:py-0 flex-col items-center justify-center space-y-4 lg:space-y-3 xl:space-y-4'>
+                <h1 className='text-4xl md:text-5xl font-bold md:pb-4 lg:pb-0 xl:pb-4 lg:py-2'>Welcome Back</h1>
+                <button className='btn btn-outline w-full lg:w-1/2 border-2 rounded-xl'>
+                    <span className='text-2xl pb-1'>
+                        <FcGoogle />
+                    </span>
+                    <span className='font-medium'>Continue with Google</span>
+                </button>
+                <Divider />
+                <div className="inputs w-full lg:w-1/2 pb-6 lg:pb-0 xl:pb-6 flex flex-col items-center space-y-2">
+                    <input
+                        onChange={handleChange}
+                        value={credentials.email}
+                        className='border-2 w-full outline-none bg-[#fafafa] border-[#c6c6c6] p-3 px-4 mx-4 rounded-xl'
+                        placeholder='Enter the email'
+                        type='email' name='email'
+                        id='email'
+                        required
+                    />
+                    <input
+                        onChange={handleChange}
+                        value={credentials.password}
+                        className='border-2 w-full outline-none bg-[#fafafa] border-[#c6c6c6] p-3 px-4 mx-4 rounded-xl'
+                        placeholder='Enter the password'
+                        type='password' name='password'
+                        id='password'
+                        required
+                    />
+                </div>
+                <button onClick={handleSignin} className='btn btn-active btn-neutral bg-black text-white w-full lg:w-1/2 rounded-xl'>
+                    <span>Log in</span>
+                </button>
+                <div className="alreadyText">
+                    No account? <span className='font-bold'>
+                        <Link to='/sign-up'>
+                            Create an account
+                        </Link>
+                    </span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const Divider = () => {
+    return (
+        <div className="or w-full lg:w-1/2 py-4 lg:py-0 xl:py-4 flex items-center space-x-4">
+            <div className='w-full h-[0.075rem] bg-gray-300' />
+            <div className='text-xl pb-1'>or</div>
+            <div className='w-full h-[0.075rem] bg-gray-300' />
+        </div>
+    )
+}
+
+export default SignIn
